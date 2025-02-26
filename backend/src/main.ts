c4b01111dceca0be026a7403e20f116f33f4747c
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-02-26 20:50:08 +0530

    changed the app.listen port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,12 +7,6 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
   });
   await app.listen(process.env.PORT ?? 3001);
-  app.enableCors({
-    origin: 'http://localhost:4000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
