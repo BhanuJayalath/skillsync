80834a7d0aad5c8c458baadab4cfbcacc638e63b
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-02-28 18:42:29 +0530

    moving the profile edit section to settings

--- frontend/src/app/userProfile/page.tsx
+++ frontend/src/app/userProfile/page.tsx
@@ -74,7 +74,7 @@ export default function UserProfile () {
         fetchUserDetails();
         //checking the availability of the window
         if(typeof window !== 'undefined'){
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 5; i++) {
                 const section = document.querySelector<HTMLDivElement>(`#tab-${i}`);
                 if (section) {
                     section.style.display = i === activeTab ? 'block' : 'none';
@@ -332,6 +332,66 @@ export default function UserProfile () {
                                     }
                                 }}>Print Preview / Save as PDF
                                 </button>
+                            </section>
+                            <section id="tab-4" className={styles.userInfo}>
+                                <div className={styles.welcomeMessage}>Welcome, {user.userName}</div>
+                                <div className={styles.userDetails}>
+                                    <div className={styles.profilePic}>
+                                        {user.avatar ? (
+                                            <span className={styles.userAvatar}>{user.avatar}</span>
+                                        ) : (
+                                            <span><Image src={"/user/userIcon.svg"} alt="userIcon"
+                                                         width={100} height={0} className={styles.userAvatar}/></span>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <strong>{user.userName}</strong>
+                                        <p>{user.email}</p>
+                                    </div>
+                                    <button className={styles.editButton}>Edit</button>
+                                </div>
+                                {/*form section*/}
+                                <form className={styles.form}>
+                                    <div>
+                                        <label>Full Name</label>
+                                        <input placeholder="Your First Name"/>
+                                    </div>
+                                    <div>
+                                        <label>Display Name</label>
+                                        <input placeholder="Your Display Name"/>
+                                    </div>
+                                    <div>
+                                        <label>Gender</label>
+                                        <select>
+                                            <option>Your Gender</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label>Country</label>
+                                        <select>
+                                            <option>Your Country</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label>Language</label>
+                                        <select>
+                                            <option>Languages</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <label>Time Zone</label>
+                                        <select>
+                                            <option>Time Zone</option>
+                                        </select>
+                                    </div>
+                                </form>
+
+                                <div className={styles.emailSection}>
+                                    <h3>My Email Address</h3>
+                                    <p>{user.email}</p>
+                                    <p>1 month ago</p>
+                                    <button>+Add Email Address</button>
+                                </div>
                             </section>
                         </section>
                     </div>
