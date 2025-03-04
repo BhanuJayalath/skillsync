1954f9900b1f9c5176d230da116c11bec95004d7
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-04 08:06:34 +0530

    Used hooks (useState) for the username & password

--- frontend/src/app/adminPage/SignInForAdmin.tsx
+++ frontend/src/app/adminPage/SignInForAdmin.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import styles from "./adminPage.module.css"
 
 export default function SignInForAdmin() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
   return (
     <div className={styles.signinContainer}>
       <h2>Admin Sign In</h2>
@@ -8,10 +12,14 @@ export default function SignInForAdmin() {
       <input 
         type="text" 
         placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <input 
         type="password" 
         placeholder="Password" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
     </div>
   )
