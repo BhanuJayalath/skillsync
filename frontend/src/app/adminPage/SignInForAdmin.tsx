2573487c8f22be6cb0a54ed306cb23b7d51f118a
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-04 08:18:22 +0530

    Implimented handleLogin functionality

--- frontend/src/app/adminPage/SignInForAdmin.tsx
+++ frontend/src/app/adminPage/SignInForAdmin.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import styles from "./adminPage.module.css"
+import { useRouter } from "next/router";
 
 export default function SignInForAdmin() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const router = useRouter();
+
+    const handleLogin = () => {
+        const adminUserName = "adminSkillSync";
+        const adminPassword = "passwordSkillSync";
+
+        if (username === adminUserName && password === adminPassword){
+            localStorage.setItem("isAuthenticated", "true");
+            router.push("page");
+        } else {
+            setError("Invalid username or password");
+        }
+    }
 
   return (
     <div className={styles.signinContainer}>
