6f2eb97cd5c58c723889e5939ce50c3bedd1135b
Ashmirah <ashmirah03@gmail.com>
2025-03-05 12:25:57 +0530

    deleted backend nest.js login files

--- backend/src/auth/models/user.schema/user.schema.spec.ts
+++
@@ -1,7 +0,0 @@
-import { User, UserSchema } from './user.schema';
-
-describe('UserSchema', () => {
-  it('should be defined', () => {
-    expect(UserSchema).toBeDefined(); // ✅ Correct way to test Mongoose Schema
-  });
-});
