f6bd9c8d362783d6e9027895fa8096511c285bbe
Ashmirah <ashmirah03@gmail.com>
2025-03-05 07:06:01 +0530

    resolve conflict 2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,16 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserProfileModule } from './user-profile/user-profile.module';
-import { UserProfileController } from './user-profile/user-profile.controller';
 import { JobRecommendationModule } from './job-recommendation/job-recommendation.module';
-@Module({
-  imports: [UserProfileModule, JobRecommendationModule],
-  controllers: [AppController, UserProfileController],
 import { RecruiterModule } from './recruiter/recruiter.module';
 import { MocktestModule } from './mocktest/mocktest.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [RecruiterModule, MocktestModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      autoIndex: true,
+      autoCreate: true,
+    }),
+    RecruiterModule, 
+    MocktestModule,
+    UserProfileModule, 
+    JobRecommendationModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
