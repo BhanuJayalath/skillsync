c1e829e78c25f2a95531e32620a98ce627c7c65e
Bhanu Jayalath <bhanujayalath2003@gmail.com>
2025-03-06 00:27:13 +0530

    updated gitignore file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for requests from Next.js
   app.enableCors({
-    origin: 'https://skillsync-git-userprofile-skill-syncs-projects.vercel.app',
+    origin: process.env.CORS_ORIGIN,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
