81b33bbd09a829d3f729017c729c6982999bca9c
Bhanu Jayalath <bhanujayalath2003@gmail.com>
2025-03-06 16:19:52 +0530

    working on dashboard section

--- frontend/src/app/recruiter-profile/Dashboard.tsx
+++ frontend/src/app/recruiter-profile/Dashboard.tsx
@@ -1,29 +1,62 @@
 import Tab from "./Tab";
 import ResultTab from "./ResultTab";
 import styles from "../assets/styles/recruiter.module.css";
+import { useEffect, useState } from "react";
 export default function Dashboard() {
+  const [jobs, setJobs] = useState([]);
+  const [profiles, setProfiles] = useState([]);
+  const [testResults, setTestResults] = useState([]);
+
+  const users = [
+    {
+      userId: "1",
+      jobId: "1",
+      tests: [
+        { testId: "1", result: 25 },
+        { testId: "2", result: 50 },
+      ],
+    },
+    {
+      userId: "2",
+      jobId: "2",
+      tests: [
+        { testId: "1", result: 50 },
+        { testId: "2", result: 90 },
+      ],
+    },
+    {
+      userId: "3",
+      jobId: "1",
+      tests: [
+        { testId: "1", result: 30 },
+        { testId: "2", result: 60 },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    const jobSet: any = [];
+    users.map((item: any) => {
+      jobSet.push(item);
+    });
+    setJobs(jobSet);
+    console.log(jobSet);
+  }, []);
+
+  // function performance() {}
   return (
     <div id={styles.contentContainer1}>
       <div id={styles.topGraded}>
-        <h1>Top Graded</h1>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
-        <div id={styles.topGradedContainer}>
-          <Tab />
-        </div>
+        <h1>Job Post</h1>
+        <>
+          {jobs.map((item: any) => {
+            return (
+              <div id={styles.topGradedContainer}>
+                <Tab />
+              </div>
+            );
+          })}
+        </>
       </div>
       <div id={styles.profiles}>
         <h1>Profiles</h1>
