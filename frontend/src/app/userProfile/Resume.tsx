5620889cff495d8e0d33cc705c517be2f08d6388
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-06 19:51:32 +0530

    add a useEffect to update the cv based on the availability of the user details

--- frontend/src/app/userProfile/Resume.tsx
+++ frontend/src/app/userProfile/Resume.tsx
@@ -45,7 +45,49 @@ interface ResumeProps {
 }
 
 const Resume = ({user, removeEducation, removeExperience, updateNestedChanges}: ResumeProps) => {
+    useEffect(() => {
+        if (!user) return; // Prevent running if user is not defined
 
+        const allJobNamesAreEmpty = user.experience?.every((job) => job.jobName === "") ?? true;
+        const allCourseNamesAreEmpty = user.education?.every((course) => course.courseName === "") ?? true;
+
+        if (typeof window !== 'undefined') {
+            const educationElement = document.getElementById('educationSection');
+            const skillElement = document.getElementById('skillSection');
+            const experienceElement = document.getElementById('experienceSection');
+            const summaryElement = document.getElementById('summarySection');
+
+            user.experience?.forEach((job, index) => {
+                if (!job.jobName) {
+                    updateNestedChanges(index, "experience");
+                }
+            });
+
+            if (allJobNamesAreEmpty && experienceElement) {
+                experienceElement.style.display = 'none';
+            }
+
+            user.education?.forEach((course, index) => {
+                if (!course.courseName) {
+                    updateNestedChanges(index, "education");
+                }
+            });
+
+            if (allCourseNamesAreEmpty && educationElement) {
+                educationElement.style.display = 'none';
+            }
+
+            if (!user.skills?.length && skillElement) {
+                skillElement.style.display = 'none';
+            }
+
+            if (!user.cvSummary?.length && summaryElement) {
+                summaryElement.style.display = 'none';
+            }
+
+            console.log(user.experience);
+        }
+    }, [user?.cvSummary?.length, user?.skills?.length, user?.education?.length, user?.experience?.length]);
 
     return (
         <section className={styles.cvSection}>
