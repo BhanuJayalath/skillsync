d7e82d4faa547242480e3ac3b4717d647279817e
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-12 14:11:56 +0530

    Implemented the CRUD operations in service.ts

--- backend/src/admin-page/admin-page.service.ts
+++ backend/src/admin-page/admin-page.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+// import { User } from ;
 
 @Injectable()
-export class AdminPageService {}
+export class AdminPageService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async create(userData: Partial<User>): Promise<User> {
+    const user = this.userRepository.create(userData);
+    return this.userRepository.save(user);
+  }
+
+  async update(id: number, userData: Partial<User>): Promise<User> {
+    await this.userRepository.update(id, userData);
+    return this.findOne(id);
+  }
+
+  async delete(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
