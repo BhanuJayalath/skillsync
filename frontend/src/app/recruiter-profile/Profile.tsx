29f31f53d8a6e2eb2bbebe6d47b649d3b52faf17
Bhanu Jayalath <bhanujayalath2003@gmail.com>
2025-03-13 16:39:37 +0530

    working on styling on main containers

--- frontend/src/app/recruiter-profile/Profile.tsx
+++ frontend/src/app/recruiter-profile/Profile.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 export default function Profile() {
   const [update, setUpdate] = useState(false);
   const [companyName, setCompanyName] = useState("WS02");
-  const [email, setEmail] = useState("company@gmail.com");
+  const [email, setEmail] = useState("ws02@gmail.com");
   const [description, setDescription] = useState(
     "WSO2 is a leading open-source technology company that provides enterprise-grade solutions for API management, integration, and identity and access management (IAM). Its flagship products, such as WSO2 API Manager and WSO2 Identity Server, enable businesses to build, deploy, and secure digital services efficiently. With a strong focus on cloud-native and microservices architectures, WSO2 helps organizations streamline their digital transformation by offering scalable and customizable platforms. It supports various industry standards and offers both on-premise and cloud deployment options, making it a preferred choice for enterprises seeking robust and flexible integration solutions."
   );
@@ -14,29 +14,54 @@ export default function Profile() {
   );
   return (
     <section id={styles.companyProfile}>
-      <button
-        onClick={() => {
-          setUpdate(!update);
-        }}
-      >
-        <Image
-          alt="menu-icon"
-          width={23}
-          height={23}
-          src="/recruiter/menu.svg"
-        />
-      </button>
+      {update ? (
+        <header>
+          <h1>Update Your Company Info</h1>
+          <button
+            onClick={() => {
+              setUpdate(!update);
+            }}
+          >
+            <Image
+              alt="update-icon2"
+              width={20}
+              height={20}
+              src="/recruiter/update-icon2.svg"
+            />
+            Save Company Info
+          </button>
+        </header>
+      ) : (
+        <header>
+          <button
+            onClick={() => {
+              setUpdate(!update);
+            }}
+          >
+            <Image
+              alt="update-icon2"
+              width={20}
+              height={20}
+              src="/recruiter/update-icon2.svg"
+            />
+            Update Company Info
+          </button>
+        </header>
+      )}
       {update ? (
         <div id={styles.updateProfileCard}>
-          <header>Update Your Company Info</header>
           <form id={styles.profileForm}>
             <div>
               <label>Company</label>
               <input type="text"></input>
             </div>
-            <div>
+            <div id={styles.companyProfileAbout}>
               <label>About Your Company</label>
-              <input type="text"></input>
+              <textarea
+                rows={4}
+                cols={50}
+                id={styles.companyProfileAbout}
+              ></textarea>
             </div>
             <div>
               <label>Company Email</label>
