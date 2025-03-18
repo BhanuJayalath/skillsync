1fd5dba84d8944881eff80c66c1dfb501c6774a0
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-18 22:20:54 +0530

    add the assessment component and pass the user details

--- frontend/src/app/userProfile/[id]/page.tsx
+++ frontend/src/app/userProfile/[id]/page.tsx
@@ -427,6 +427,7 @@ import Careers from '@/app/job-recommendations/page';
                                                 removeExperience={removeExperience}
                                                 updateNestedChanges={updateNestedChanges}/>}
                                             {activeTab === 4 && <MockInterview/>}
+                                            {/*{activeTab === 5 && <Assessment user={user}/>}*/}
                                             {activeTab === 7 && <Settings
                                                 user={user}
                                                 handleSubmit={handleSubmit}
