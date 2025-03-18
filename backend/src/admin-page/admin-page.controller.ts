6d8307a801a8366e868b90dfeb7abd67aed4eb13
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-18 19:46:00 +0530

    Implemented the routes in controller.ts

--- backend/src/admin-page/admin-page.controller.ts
+++ backend/src/admin-page/admin-page.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('admin-page')
-export class AdminPageController {}
+export class AdminPageController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() userData: Partial<User>): Promise<User> {
+    return this.userService.create(userData);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() userData: Partial<User>): Promise<User> {
+    return this.userService.update(id, userData);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Promise<void> {
+    return this.userService.delete(id);
+  }
+}
+
