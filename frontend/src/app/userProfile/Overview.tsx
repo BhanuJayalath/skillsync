c81de3aac36ee77d8960a7ca168deebbf0ae12d9
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-18 21:28:18 +0530

    created the nav bar add the profile

--- frontend/src/app/userProfile/Overview.tsx
+++ frontend/src/app/userProfile/Overview.tsx
@@ -1,48 +1,56 @@
 import styles from "@/app/userProfile/user.module.css";
 import Image from "next/image";
-import React from "react";
+import React, {useState} from "react";
 
 interface User {
     userName: string;
     avatar: string;
     email: string;
-    selectedJob: selectedJob;
+    selectedJob: SelectedJob;
     contact: string;
     linkedIn: string;
     gitHub: string;
 }
 
-interface selectedJob{
+interface SelectedJob{
     jobTitle:string;
     jobId:string;
 }
 
 const Overview = ({ user }: { user: User }) => {
+    const [activeTab, setActiveTab] = useState(0);
     return (
-        <section className={styles.userInfo}>
-            <div className={styles.welcomeMessage}>Welcome, {user.userName}</div>
-            <div className={styles.userDetails}>
-                <div className={styles.profilePic}>
-                    {user.avatar ? (
-                        <span><Image src={user.avatar} alt="userIcon"
-                                     width={100} height={100} className={styles.userAvatar}/></span>
-                    ) : (
-                        <span><Image src={"/user/userIcon.svg"} alt="userIcon"
-                                     width={100} height={100} className={styles.userAvatar}/></span>
-                    )}
+        <section className={styles.overviewContainer} >
+            <nav className={styles.overviewNavBar}>
+                <ul>
+                    <li
+                        onClick={() => setActiveTab(0)}
+                        className={activeTab === 0 ? styles.activeLink : ''}
+                    > Profile
+                    </li>
+                </ul>
+            </nav>
+            <section className={styles.overviewUserContainer}>
+                <div className={styles.userProfile}>
+                    <div className={styles.profilePicContainer}>
+                        {user.avatar ? (
+                            <span><Image src={user.avatar} alt="userIcon"
+                                         width={100} height={100} className={styles.userAvatar}/></span>
+                        ) : (
+                            <span><Image src={"/user/userIcon.svg"} alt="userIcon"
+                                         width={100} height={100} className={styles.userAvatar}/></span>
+                        )}
+                    </div>
+                    <div>
+                        <strong>{user.userName}</strong>
+                        <p>{user.selectedJob?.jobTitle}</p>
+                    </div>
                 </div>
-                <div>
-                    <strong>{user.userName}</strong>
-                    <p>{user.email}</p>
-                </div>
-            </div>
-            <div>
-                <p>Job Role: {user.selectedJob?.jobTitle}</p>
-                <p>Contact: {user.contact}</p>
-                <p>LinkedIn: {user.linkedIn}</p>
-                <p>Github: {user.gitHub}</p>
-            </div>
-            <div className={styles.userFooter}>go to settings to edit the user profile</div>
+                }
+
+
+                {/*<div className={styles.userFooter}>go to settings to edit the user profile</div>*/}
+            </section>
         </section>
     );
 };
