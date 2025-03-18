6abc828473c933032e04059ddaaa51e5b45a6a15
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-18 20:22:53 +0530

    Updated the controller.ts

--- backend/src/admin-page/admin-page.controller.ts
+++ backend/src/admin-page/admin-page.controller.ts
@@ -1,34 +1,33 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
-import { UserService } from './user.service';
-import { User } from './user.entity';
+import { UserService } from './admin-page.service';
+import { Admin } from './admin-page.schema';
 
 @Controller('admin-page')
-export class AdminPageController {
+export class AdminController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  findAll(): Promise<User[]> {
+  findAll(): Promise<Admin[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number): Promise<User> {
+  findOne(@Param('id') id: string): Promise<Admin> {
     return this.userService.findOne(id);
   }
 
   @Post()
-  create(@Body() userData: Partial<User>): Promise<User> {
+  create(@Body() userData: Partial<Admin>): Promise<Admin> {
     return this.userService.create(userData);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() userData: Partial<User>): Promise<User> {
+  update(@Param('id') id: string, @Body() userData: Partial<Admin>): Promise<Admin> {
     return this.userService.update(id, userData);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number): Promise<void> {
+  delete(@Param('id') id: string): Promise<void> {
     return this.userService.delete(id);
   }
 }
-
