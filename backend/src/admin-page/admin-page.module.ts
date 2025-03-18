713c8314a6508b439c82f2b9bcc29acd605d5b3d
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-18 19:56:16 +0530

    Updated the module.ts to use MongoDB Database

--- backend/src/admin-page/admin-page.module.ts
+++ backend/src/admin-page/admin-page.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AdminPageService } from './admin-page.service';
 import { AdminPageController } from './admin-page.controller';
+import { Admin, AdminSchema } from './admin-page.schema';
+
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }])],
   providers: [AdminPageService],
-  controllers: [AdminPageController]
+  controllers: [AdminPageController],
 })
 export class AdminPageModule {}
