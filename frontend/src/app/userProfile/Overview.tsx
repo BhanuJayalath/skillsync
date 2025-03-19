58290894fa56018522e01838c10cbe145d65dc0e
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-19 18:43:03 +0530

    hide the portfolio icon when its undefined

--- frontend/src/app/userProfile/Overview.tsx
+++ frontend/src/app/userProfile/Overview.tsx
@@ -93,7 +93,9 @@ const Overview = ({ user }: { user: User }) => {
                     <p className={styles.schoolName}>{user?.education[0]?.schoolName}</p>
                     <p className={styles.graduationYear}>{user?.education[0]?.startDate} {user.education[0]?.endDate && `-${user.education[0]?.endDate}`}</p>
                     <p className={styles.jobResponsibilities}>{user?.education[0]?.description}</p>
-
+                    <p>{user?.portfolio && <Image src={"/user/portfolio.svg"} alt="portfolio"
+                              className={styles.overviewIcons}
+                              width={20} height={20}/>}{user?.portfolio}</p>
                 </div>}
                 {activeTab === 2 && <div className={styles.userDetails}>
 
