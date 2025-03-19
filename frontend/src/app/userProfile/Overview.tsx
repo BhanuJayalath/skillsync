e5dd6f358e859fe32314c02c053b7c3537e9facc
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-19 10:42:26 +0530

    add the optional chaining operator to the user to access nested objects safely inside the user

--- frontend/src/app/userProfile/Overview.tsx
+++ frontend/src/app/userProfile/Overview.tsx
@@ -60,8 +60,8 @@ const Overview = ({ user }: { user: User }) => {
             <section className={styles.overviewUserContainer}>
                 <div className={styles.userProfile}>
                     <div className={styles.profilePicContainer}>
-                        {user.avatar ? (
-                            <span><Image src={user.avatar} alt="userIcon"
+                        {user?.avatar ? (
+                            <span><Image src={user?.avatar} alt="userIcon"
                                          width={100} height={100} className={styles.userAvatar}/></span>
                         ) : (
                             <span><Image src={"/user/userIcon.svg"} alt="userIcon"
@@ -69,37 +69,37 @@ const Overview = ({ user }: { user: User }) => {
                         )}
                     </div>
                     <div>
-                        <strong>{user.userName}</strong>
-                        <p>{user.selectedJob?.jobTitle}</p>
+                        <strong>{user?.userName}</strong>
+                        <p>{user?.selectedJob?.jobTitle}</p>
                     </div>
                 </div>
                 {activeTab === 0 && <div className={styles.userDetails}>
                     <p><Image src={"/user/email.svg"} alt="email"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.email}</p>
+                              width={20} height={20}/>{user?.email}</p>
                     <p><Image src={"/user/phone.svg"} alt="phone"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.contact}</p>
+                              width={20} height={20}/>{user?.contact}</p>
                     <p><Image src={"/user/linkedin.svg"} alt="linkedin"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.linkedIn}</p>
+                              width={20} height={20}/>{user?.linkedIn}</p>
                     <p><Image src={"/user/github.svg"} alt="github"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.gitHub}</p>
+                              width={20} height={20}/>{user?.gitHub}</p>
                 </div>}
                 {activeTab === 1 && <div className={styles.userDetails}>
-                    <h3 className={styles.degreeTitle}>{user.education[0].courseName}</h3>
-                    <p className={styles.schoolName}>{user.education[0].schoolName}</p>
-                    <p className={styles.graduationYear}>{user.education[0].startDate} {user.education[0].endDate && `-${user.education[0].endDate}`}</p>
-                    <p className={styles.jobResponsibilities}>{user.education[0].description}</p>
-                    <p><Image src={"/user/email.svg"} alt="email"
+                    <h3 className={styles.degreeTitle}>{user?.education[0]?.courseName}</h3>
+                    <p className={styles.schoolName}>{user?.education[0]?.schoolName}</p>
+                    <p className={styles.graduationYear}>{user?.education[0]?.startDate} {user.education[0]?.endDate && `-${user.education[0]?.endDate}`}</p>
+                    <p className={styles.jobResponsibilities}>{user?.education[0]?.description}</p>
+                    <p><Image src={"/user/"} alt="portfolio"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.portfolio}</p>
+                              width={20} height={20}/>{user?.portfolio}</p>
                 </div>}
                 {activeTab === 2 && <div className={styles.userDetails}>
                     <p><Image src={"/user/email.svg"} alt="email"
                               className={styles.overviewIcons}
-                              width={20} height={20}/>{user.email}</p>
+                              width={20} height={20}/>{user?.email}</p>
                 </div>}
             </section>
         </section>
