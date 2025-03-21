adac87d203cb5807a76de8c08a73bfbdc91823c5
Ashmirah <ashmirah03@gmail.com>
2025-03-21 16:41:09 +0530

    added the user skill route file

---
+++ frontend/src/app/api/jobs/userSkills/route.ts
@@ -0,0 +1,39 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import TestScore from "@/models/testScore";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = request.nextUrl.searchParams.get("user-id");
+
+    if (!userId) {
+      return NextResponse.json(
+        { error: "User ID is required" },
+        { status: 400 }
+      );
+    }
+
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const userScore = await TestScore.findOne({ userId });
+
+    let skills: string[] = [];
+    if (userScore && userScore.overallScore > 4) {
+      skills = (userScore.skillScores as { skill: string }[]).map((skillScore) => skillScore.skill);
+    }
+
+    return NextResponse.json(
+      { user: { skills } },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
