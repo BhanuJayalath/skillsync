97bcd2dbfc7156a9d332415ec843fe428355ca80
Pesandu De Alwis <pesandu.sl@gmail.com>
2025-03-22 22:11:45 +0530

    Connected Next.js Frontend to nest.js api

--- backend/src/admin-page/admin-page.module.ts
+++ backend/src/admin-page/admin-page.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AdminModule } from './admin-page.module';
 import { AdminService } from './admin-page.service';
 import { AdminController } from './admin-page.controller';
 import { Admin, AdminSchema } from './admin-page.schema';
