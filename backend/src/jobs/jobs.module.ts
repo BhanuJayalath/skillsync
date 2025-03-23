cc2dc93973a807e1d855a5502cb81b638af59262
DishikaVeruwantha <veruwanthadishika@gmail.com>
2025-03-23 19:22:30 +0530

    add relevant comments to the jobs.module.ts

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -5,7 +5,9 @@ import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 
 @Module({
+  // Import the Job model and JobSchema
   imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])],
+  // Define the JobsController and JobsService as providers
   controllers: [JobsController],
   providers: [JobsService],
   exports: [JobsService],
